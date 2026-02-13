605e273d8d9bd5817f04edad038509e9ad960f25
Jay Austin Bobis <bobisjayaustin5@gmail.com>
2026-02-13 14:09:49 +0800

    Implement admobs banner

---
+++ composeApp/src/commonMain/kotlin/com/johnreicabunas/clockwise/presentation/components/AdBanner.kt
@@ -0,0 +1,7 @@
+package com.johnreicabunas.clockwise.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+expect fun AdBanner(modifier: Modifier = Modifier)
