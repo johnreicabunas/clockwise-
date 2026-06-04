8ba55bb20c3ee8a0b7674999844e150242e081f8
John Rei Bobis Cabuñas <johnreicabunas@gmail.com>
2026-06-04 20:40:13 +0800

    Bump release version to 1.1.8

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -77,8 +77,8 @@ android {
         applicationId = "com.johnreicabunas.clockwise"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 7
-        versionName = "1.0.1"
+        versionCode = 9
+        versionName = "1.1.8"
     }
     packaging {
         resources {
